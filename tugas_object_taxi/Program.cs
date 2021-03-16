fe53fbf712e6cda6374f0e466dada030d938270f
Aziz <abdullathifaziz@gmail.com>
2021-03-16 16:20:59 +0700

    Class dan Object Tazi

---
+++ tugas_object_taxi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tugas_class_object_taxi
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // objek taxi
+            Taxi taxi = new Taxi();
+
+            // set nilai properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 10;
+
+            // pemanggilan methode
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOfPassanger();
+
+            Console.Read();
+        }
+    }
+}
---
+++ tugas_object_taxi/Taxi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace tugas_class_object_taxi
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassanger { get; set; }
+
+        // method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("OnDuty : {0}", OnDuty);
+            Console.WriteLine("Number Of Passanger : {0}\n", NumPassanger);
+        }
+
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOfPassanger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang\n", DriverName);
+        }
+
+    }
+}
